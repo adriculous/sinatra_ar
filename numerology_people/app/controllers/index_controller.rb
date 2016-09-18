8b3a43357d893b2690a5c4757232bcdfc943a82b
Adri P <adri@adrimarie.us>
2016-09-18 13:49:05 -0700

    changed some method names to match numerology_spec.rb

--- numerology_people/app/controllers/index_controller.rb
+++ numerology_people/app/controllers/index_controller.rb
@@ -3,8 +3,8 @@
 require 'sinatra'
 
 # Okay, let's make up a method for the results!
-def get_birth_num(birthday)
-     number = birthday[0].to_i + birthday[1].to_i + birthday[2].to_i + birthday[3].to_i + birthday[4].to_i + birthday[5].to_i + birthday[6].to_i + birthday[7].to_i
+def get_birth_path_num(birthdate)
+     number = birthdate[0].to_i + birthdate[1].to_i + birthdate[2].to_i + birthdate[3].to_i + birthdate[4].to_i + birthdate[5].to_i + birthdate[6].to_i + birthdate[7].to_i
 
 # convert the number to string, use array format
 number = number.to_s
@@ -20,26 +20,26 @@ number = number[0].to_i + number[1].to_i
 end
 
 # case statement for the number meanings
-def get_message(birth_num)
-     case birth_num
+def get_message(birth_path_num)
+     case birth_path_num
           when 1
-               message = "Your numerology number is #{birth_num}.\nOne is the leader. The number one indicates the ability to stand alone, and is a strong vibration. Ruled by the Sun."
+               message = "Your numerology number is #{birth_path_num}.\nOne is the leader. The number one indicates the ability to stand alone, and is a strong vibration. Ruled by the Sun."
           when 2
-               message = "Your numerology number is #{birth_num}.\nThis is the mediator and peace-lover. The number two indicates the desire for harmony. It is a gentle, considerate, and sensitive vibration. Ruled by the Moon."
+               message = "Your numerology number is #{birth_path_num}.\nThis is the mediator and peace-lover. The number two indicates the desire for harmony. It is a gentle, considerate, and sensitive vibration. Ruled by the Moon."
           when 3
-               message = "Your numerology number is #{birth_num}.\nNumber Three is a sociable, friendly, and outgoing vibration. Kind, positive, and optimistic, Three's enjoy life and have a good sense of humor. Ruled by Jupiter."
+               message = "Your numerology number is #{birth_path_num}.\nNumber Three is a sociable, friendly, and outgoing vibration. Kind, positive, and optimistic, Three's enjoy life and have a good sense of humor. Ruled by Jupiter."
           when 4
-               message = "Your numerology number is #{birth_num}.\nThis is the worker. Practical, with a love of detail, Fours are trustworthy, hard-working, and helpful. Ruled by Uranus."
+               message = "Your numerology number is #{birth_path_num}.\nThis is the worker. Practical, with a love of detail, Fours are trustworthy, hard-working, and helpful. Ruled by Uranus."
           when 5
-               message = "Your numerology number is #{birth_num}.\nThis is the freedom lover. The number five is an intellectual vibration. These are 'idea' people with a love of variety and the ability to adapt to most situations. Ruled by Mercury."
+               message = "Your numerology number is #{birth_path_num}.\nThis is the freedom lover. The number five is an intellectual vibration. These are 'idea' people with a love of variety and the ability to adapt to most situations. Ruled by Mercury."
           when 6
-               message = "Your numerology number is #{birth_num}.\nThis is the peace lover. The number six is a loving, stable, and harmonious vibration. Ruled by Venus."
+               message = "Your numerology number is #{birth_path_num}.\nThis is the peace lover. The number six is a loving, stable, and harmonious vibration. Ruled by Venus."
           when 7
-               message = "Your numerology number is #{birth_num}.\nThis is the deep thinker. The number seven is a spiritual vibration. These people are not very attached to material things, are introspective, and generally quiet. Ruled by Neptune."
+               message = "Your numerology number is #{birth_path_num}.\nThis is the deep thinker. The number seven is a spiritual vibration. These people are not very attached to material things, are introspective, and generally quiet. Ruled by Neptune."
           when 8
-               message = "Your numerology number is #{birth_num}.\nThis is the manager. Number Eight is a strong, successful, and material vibration. Ruled by Saturn."
+               message = "Your numerology number is #{birth_path_num}.\nThis is the manager. Number Eight is a strong, successful, and material vibration. Ruled by Saturn."
           when 9
-               message = "Your numerology number is #{birth_num}.\nThis is the teacher. Number Nine is a tolerant, somewhat impractical, and sympathetic vibration. Ruled by Mars."
+               message = "Your numerology number is #{birth_path_num}.\nThis is the teacher. Number Nine is a tolerant, somewhat impractical, and sympathetic vibration. Ruled by Mars."
           else
                message = "I am pretty sure that you were not born on the leap year date, because you should still get a Numerology number. Either you entered your birthddate in the incorrect format or you are not originally from Earth, you should have a number between 1-9."
      end
@@ -51,13 +51,13 @@ get '/static' do
      erb :static
 end
 
-get '/:birthday' do
+get '/:birthdate' do
      setup_index_view
 end
 
-get '/message/:birth_num' do
-   birth_num = params[:birth_num].to_i
-   @message = get_message(birth_num)
+get '/message/:birth_path_num' do
+   birth_path_num = params[:birth_path_num].to_i
+   @message = get_message(birth_path_num)
    erb :index
 end
 
@@ -67,10 +67,10 @@ end
 
 post '/' do
      setup_index_view
-     birthday = params[:birthday].gsub("-","")
-     if valid_birthday(birthday)
-          birth_num = get_birth_num(birthday)
-          redirect "/message/#{birth_num}"
+     birthdate = params[:birthdate].gsub("-","")
+     if valid_birthdate(birthdate)
+          birth_path_num = get_birth_path_num(birthdate)
+          redirect "/message/#{birth_path_num}"
      else
           @error = "Eep!  Your valid birthdate should be in the form of mmddyyyy. Try again!"
           erb :form
@@ -78,13 +78,13 @@ post '/' do
 end
 
 def setup_index_view
-     birthday = params[:birthday]
-     birth_num = get_birth_num(birthday)
-     @message = get_message(birth_num)
+     birthdate = params[:birthdate]
+     birth_path_num = get_birth_path_num(birthdate)
+     @message = get_message(birth_path_num)
           erb :index
 end
 
-def valid_birthday(input)
+def valid_birthdate(input)
   if(input.length == 8 && !input.match(/^[0-9]+[0-9]$/).nil?)
     true
   else
