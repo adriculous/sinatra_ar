fd0c3898a73815739fbbf02d17a6f0505d6c6eb0
Adri P <adri@adrimarie.us>
2016-09-24 21:37:59 -0700

    started partials assignment with errors?

--- numerology_people/app/controllers/people_controller.rb
+++ numerology_people/app/controllers/people_controller.rb
@@ -7,7 +7,7 @@ get '/new' do
      erb :"/people/new"
 end
 
-post '/people'  
+post '/people' do 
     if params[:birthdate].include?("-")
       birthdate = params[:birthdate]
     else
@@ -16,6 +16,28 @@ post '/people'
   
     person = Person.create(first_name: params[:first_name], last_name: params[:last_name], birthdate: birthdate)
     redirect "/people/#{person.id}"
+end
+
+get '/people/:id/edit' do
+     @person = Person.find(params[:id])
+     erb :'/people/edit'
+end
+
+put '/people/:id' do
+     # update first_name, last_name
+     person = Person.find(params[:id])
+     person.first_name = params[:first_name]
+     person.last_name = params[:last_name]
+     person.birthdate = params[:birthdate]
+     person.save
+     redirect "/people/#{person.id}"
+end
+
+delete '/people/:id' do
+     # delete record!!
+     person = Person.find(params[:id])
+     person.delete
+     redirect "/people"
 end
 
 get '/people/:id' do
